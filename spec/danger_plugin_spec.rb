564b5742a76e8efe4b1d82ba394cab6fd2623b4c
Ryosuke Ito <rito.0305@gmail.com>
2022-12-13 06:36:51 +0900

    Add specs for private methods

--- spec/danger_plugin_spec.rb
+++ spec/danger_plugin_spec.rb
@@ -120,4 +120,43 @@ describe Danger::DangerMailmap do # rubocop:disable RSpec/FilePath
       )
     end
   end
+
+  describe '#mailmap_script' do
+    let(:absolute_path) { File.expand_path('.mailmap') }
+    let(:emails) { %w[0@example.com 1@example.com] }
+
+    it 'outputs shell script template' do
+      expect(mailmap.send(:mailmap_script, absolute_path, emails)).to eq <<~SHELL.rstrip
+        echo 'Correct Name <0@example.com>' >> .mailmap
+        echo 'Correct Name <1@example.com>' >> .mailmap
+      SHELL
+    end
+  end
+
+  describe '#filter_branch_script' do
+    let(:emails) { %w[0@example.com 1@example.com] }
+
+    it 'outputs shell script template' do
+      expect(mailmap.send(:filter_branch_script, emails)).to eq <<~SHELL.rstrip
+        git filter-branch --env-filter '
+        if [ "$GIT_AUTHOR_EMAIL" = "0@example.com" ]; then
+            GIT_AUTHOR_EMAIL="correct@example.com"
+            GIT_AUTHOR_NAME="Correct Name"
+        fi
+        if [ "$GIT_COMMITTER_EMAIL" = "0@example.com" ]; then
+            GIT_COMMITTER_EMAIL="correct@example.com"
+            GIT_COMMITTER_NAME="Correct Name"
+        fi
+        if [ "$GIT_AUTHOR_EMAIL" = "1@example.com" ]; then
+            GIT_AUTHOR_EMAIL="correct@example.com"
+            GIT_AUTHOR_NAME="Correct Name"
+        fi
+        if [ "$GIT_COMMITTER_EMAIL" = "1@example.com" ]; then
+            GIT_COMMITTER_EMAIL="correct@example.com"
+            GIT_COMMITTER_NAME="Correct Name"
+        fi
+        ' --tag-name-filter cat master...test-danger-mailmap
+      SHELL
+    end
+  end
 end
